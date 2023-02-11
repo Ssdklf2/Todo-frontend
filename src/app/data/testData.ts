9e2397594538d544f6da9d0da45009d29fff2afc
Ssdklf2 <hsnev@bk.ru>
2023-02-11 22:48:16 +0300

    изменения в тестовых данных

--- src/app/data/testData.ts
+++ src/app/data/testData.ts
@@ -10,56 +10,57 @@ export class TestData {
     {id: 4, title: 'Отдых'},
     {id: 5, title: 'Спорт'},
     {id: 6, title: 'Еда'},
-    {id: 7, title: 'Финансы'}
+    {id: 7, title: 'Финансы'},
+    {id: 8, title: 'Животные'}
   ]
 
   static priorities: Priority[] = [
-    {id: 1, title: 'Низкий', color: '#e5e5e5'},
-    {id: 2, title: 'Средний', color: '#85D1B2'},
-    {id: 3, title: 'Высокий', color: '#F1828D'},
-    {id: 4, title: 'Очень высокий', color: '#F1128D'}
+    {id: 1, title: 'Низкий', color: 'rgba(149,226,158,0.86)'},
+    {id: 2, title: 'Средний', color: '#04f2e6'},
+    {id: 3, title: 'Высокий', color: 'rgba(244,94,121,0.87)'},
+    {id: 4, title: 'Очень высокий', color: '#ff0000'}
   ]
 
   static tasks: Task[] = [
     {
       id: 1,
-      title: 'sdioghsdpjgipsdjpvohjdsiphngidshiogvjsdehnsdiobg',
-      category: TestData.categories[1],
+      title: 'Купить корм для черепашки',
+      category: TestData.categories[7],
       date: new Date('2019-12-12'),
       completed: true,
       priority: TestData.priorities[0]
     },
     {
       id: 2,
-      title: 'sdioghsdpjgipsdjpvohjdsiphngidshiogvjsdehnsdiobg',
-      category: TestData.categories[1],
+      title: 'Покормить собаку',
+      category: TestData.categories[7],
       date: new Date('2019-12-12'),
       completed: true,
       priority: TestData.priorities[0]
     },
     {
       id: 3,
-      title: 'task3',
+      title: 'Купить ингредиенты для пиццы',
       category: TestData.categories[1],
       date: new Date('2019-12-12'),
       completed: true,
-      priority: TestData.priorities[0]
+      priority: TestData.priorities[1]
     },
     {
       id: 4,
-      title: 'task4',
-      category: TestData.categories[1],
+      title: 'Сделать проект',
+      category: TestData.categories[2],
       date: new Date('2019-12-12'),
       completed: true,
-      priority: TestData.priorities[0]
+      priority: TestData.priorities[2]
     },
     {
       id: 5,
-      title: 'task5',
-      category: TestData.categories[0],
+      title: 'Купить торт на День Рождение',
+      category: TestData.categories[3],
       date: new Date('2020-12-12'),
       completed: false,
-      priority: TestData.priorities[1]
+      priority: TestData.priorities[3]
     },
   ]
 }
