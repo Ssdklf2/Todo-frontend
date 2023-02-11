2fdaa09dee1495e9ac79ef3f0912c1ebbdecb4cf
Ssdklf2 <hsnev@bk.ru>
2023-02-11 05:18:05 +0300

    добавили category component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CategoryComponent } from './components/category/category.component';
+import {AppComponent} from './app.component';
+import {CategoryComponent} from './components/category/category.component';
 
 @NgModule({
   declarations: [
@@ -15,4 +15,5 @@ import { CategoryComponent } from './components/category/category.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
